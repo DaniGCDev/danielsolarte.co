e69257d41b40f304aa9921dbd6b4cc188f14ba14
Daniel Solarte <contact@danielsolarte.com>
2021-09-26 13:21:32 -0500

    fix: internationalization

--- utils/internationalization.ts
+++ utils/internationalization.ts
@@ -1,5 +1,5 @@
 import TextsEN from '@constants/langs/en.json';
 
 export function getText(key: string): string {
-  return TextsEN[key] ?? key;
+  return (TextsEN as { [key: string]: string })[key] ?? key;
 }
