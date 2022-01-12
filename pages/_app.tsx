d31483c427a9fa71da5f22222e55d4422027e71f
Daniel Solarte <contact@danielsolarte.com>
2022-01-12 09:16:43 -0500

    feat(head): Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,18 @@ const PersonalPortfolio: React.FC<AppProps> = ({ Component, pageProps }) => (
       <link rel="icon" href="/favicon.ico" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <meta name="description" content={getText('head_description')} />
-      <title>Daniel Solarte Chaverra || Portfolio</title>
+      <meta
+        name="keywords"
+        content="Daniel Solarte,Daniel Solarte Chaverra,Rust,TypeScript,ts,js,JavaScript,work,cv,hoja de vida,software developer,desarrollador de software,Colombia"
+      />
+      {/* Twitter */}
+      <meta name="twitter:description" content={getText('head_description')} />
+      <meta name="twitter:title" content={getText('head_title')} />
+      {/* Facebook */}
+      <meta name="og:description" content={getText('head_description')} />
+      <meta name="og:site_name" content={getText('head_title')} />
+      <meta name="og:title" content={getText('head_title')} />
+      <title>{getText('head_title')}</title>
     </Head>
     <Header />
     <Component {...pageProps} />
