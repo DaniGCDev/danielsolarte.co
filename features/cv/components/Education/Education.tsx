50ff85d9ccdf132caae1d630d9056f4dd4b7bbdd
Daniel Solarte <contact@danielsolarte.com>
2022-03-14 21:32:03 -0500

    feat(cv): Add 'Diplomado en Artes de Datos' to Education

--- features/cv/components/Education/Education.tsx
+++ features/cv/components/Education/Education.tsx
@@ -4,18 +4,18 @@ import EducationBox from './Box';
 const Education: React.FC = () => (
   <Section title="Educación">
     <EducationBox
+      title="Diplomado en Artes de Datos"
+      institution="Universidad EAFIT"
+      from={new Date(2022, 2, 13)}
+      place="Medellín, Antioquia, Colombia"
+    />
+    <EducationBox
       title="Bootcamp front-end, habilidades blandas y marketing digital"
       institution="Medellín Tech Fest 2021 (Valle del software)"
       from={new Date(2021, 10, 23)}
       to={new Date(2021, 10, 27)}
       place="Medellín, Antioquia, Colombia"
     />
-    {/*<EducationBox
-      title="Tecnología en Desarrollo de Software"
-      institution="I.U. Pascual Bravo"
-      from={new Date(2021, 0)}
-      place="Medellín, Antioquia, Colombia"
-    />*/}
     <EducationBox
       title="Academia de Inglés"
       institution="English Club"
