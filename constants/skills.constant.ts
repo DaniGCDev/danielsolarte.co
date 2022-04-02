f4bdf1aa18cc34df28524c81ca8966a83820e6f5
Daniel Solarte <contact@danielsolarte.com>
2022-04-02 11:18:46 -0500

    feat(skills): Change PNG icons to SVG and add some skills

--- constants/skills.constant.ts
+++ constants/skills.constant.ts
@@ -1,26 +1,30 @@
 import ISkill from '@interfaces/skills.interface';
 
 // Icons
-import CSS3 from '@assets/images/skills/css3.png';
-import Flutter from '@assets/images/skills/flutter.png';
-import Git from '@assets/images/skills/git.png';
-import HTML5 from '@assets/images/skills/html5.png';
-import JavaScript from '@assets/images/skills/javascript.png';
-import MongoDB from '@assets/images/skills/mongodb.png';
-import MySQL from '@assets/images/skills/mysql.png';
+import CSS3 from '@assets/images/skills/css3.svg';
+import Firebase from '@assets/images/skills/firebase.svg';
+import Flutter from '@assets/images/skills/flutter.svg';
+import Git from '@assets/images/skills/git.svg';
+import GitHub from '@assets/images/skills/github.svg';
+import HTML5 from '@assets/images/skills/html5.svg';
+import JavaScript from '@assets/images/skills/javascript.svg';
+import MongoDB from '@assets/images/skills/mongodb.svg';
+import MySQL from '@assets/images/skills/mysql.svg';
 import NestJS from '@assets/images/skills/nestjs.svg';
 import NextJS from '@assets/images/skills/nextjs.png';
-import NodeJS from '@assets/images/skills/nodejs.png';
-import ReactJS from '@assets/images/skills/reactjs.png';
-import Rust from '@assets/images/skills/rust.png';
-import SASS from '@assets/images/skills/sass.png';
+import NodeJS from '@assets/images/skills/nodejs.svg';
+import ReactJS from '@assets/images/skills/reactjs.svg';
+import Rust from '@assets/images/skills/rust.svg';
+import SASS from '@assets/images/skills/sass.svg';
 import SocketIO from '@assets/images/skills/socketio.svg';
 import TypeScript from '@assets/images/skills/typescript.svg';
 
 const SkillsData: ISkill[] = [
   { title: 'CSS3', image: CSS3 },
+  { title: 'Firebase', image: Firebase },
   { title: 'Flutter', image: Flutter },
   { title: 'Git', image: Git },
+  { title: 'GitHub Actions', image: GitHub },
   { title: 'HTML5', image: HTML5 },
   { title: 'JavaScript', image: JavaScript },
   { title: 'MongoDB', image: MongoDB },
@@ -30,7 +34,7 @@ const SkillsData: ISkill[] = [
   { title: 'NodeJS', image: NodeJS },
   { title: 'ReactJS', image: ReactJS },
   { title: 'Rust', image: Rust },
-  { title: 'SASS', image: SASS },
+  { title: 'SASS/SCSS', image: SASS },
   { title: 'SocketIO', image: SocketIO },
   { title: 'TypeScript', image: TypeScript },
 ];
