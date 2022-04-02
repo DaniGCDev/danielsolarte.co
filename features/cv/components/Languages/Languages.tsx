19eb8a2f62931c87416c7810d153372694be1dc1
Daniel Solarte <contact@danielsolarte.com>
2022-04-02 11:43:49 -0500

    fix(cv): Change key prop to id in languages section

--- features/cv/components/Languages/Languages.tsx
+++ features/cv/components/Languages/Languages.tsx
@@ -3,7 +3,7 @@ import SmallBox from '../SmallBox';
 
 const Languages: React.FC = () => (
   <Section title="Idiomas">
-    <SmallBox key="languages" texts={['Español (Nativo)', 'Inglés (A2)']} />
+    <SmallBox id="languages" texts={['Español (Nativo)', 'Inglés (A2)']} />
   </Section>
 );
 
